9bc226af8913fed9611b262c2481fc207f9442c1
Hlompho Sibeko <hlomphosibeko2@gmail.com>
2024-08-06 12:30:54 +0000

    Add id to the questions, display my questions on javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,18 @@
  */
 let mySafariQuestions = ['1. What is the name of the above animal?', '2. What is its habitat?', 
     '3. What color is it?', '4. How many legs does it have?', '5. Does it lay eggs?', 
-    '6. Is it fast or slow?', '7. How does it move?']
+    '6. Is it fast or slow?', '7. How does it move?'];
+function displayQuestion(animalQuestion1, animalQuestion2, animalQuestion3, animalQuestion4, animalQuestion5, animalQuestion6, animalQuestion7) {
+    document.getElementById('animalQuestion1').textContent = animalQuestion1;
+    document.getElementById('animalQuestion1').textContent = animalQuestion2;
+    document.getElementById('animalQuestion1').textContent = animalQuestion3;
+    document.getElementById('animalQuestion1').textContent = animalQuestion4;
+    document.getElementById('animalQuestion1').textContent = animalQuestion5;
+    document.getElementById('animalQuestion1').textContent = animalQuestion6;
+    document.getElementById('animalQuestion1').textContent = animalQuestion7;
+}
 
-                    
-
+                
 /**
  * Create a variable that will hold the index number of the question you are currently displaying
  */
@@ -17,8 +25,15 @@ let indexQuestion = mySafariQuestions[2];
  * Create a function that will generate a random number from the array and store it in the global variable in number 2
  */
 
-function randomQuestion() {
+function randomQuestion(gameType) {
+    let mySafariQuestions = Math.floor(Math.random);
 
+    if (gameType === "hidden-question") {
+       displayQuestion(mySafariQuestions);
+    } else {
+        alert(`Unkown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}. Aborting!`;
+    }
 }
 /**
  * Create a function that will pull a question from the array of objects
@@ -42,10 +57,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                randomQuestion(gameType);
             }
         })
     }
+
+    randomQuestion("hidden-question");
 })
 /**
  * when the click is registered, you want to compare the correct answer with the users answers
